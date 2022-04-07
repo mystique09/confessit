92a5348cb02a84b01be3ac3d6e3b292f2b20984c
mystique09 <knightkrypto7@gmail.com>
2022-04-07 15:06:35 +0800

    add signup form basic layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+const payload = {
+    username: "",
+    password: ""
+};
+
+const submitForm = () => {
+    console.log(payload)
+}
+</script>
+
+<div class="main">
+    <h1 class="heading">
+        Receive message from strangers
+    </h1>
+    <form on:submit|preventDefault={submitForm}>
+            <h2>Start receiving messages</h2>
+            <div class="input_group">
+                <label for="username">Username</label>
+                <input type="text" bind:value={payload.username} required>
+            </div>
+            <div class="input_group">
+                <label for="password">Password</label>
+                <input type="password" bind:value={payload.password} required>
+            </div>
+            <button type="submit">Submit</button>
+    </form>
+</div>
+
+<style lang="postcss">
+    h1 {
+        @apply font-semibold text-4xl my-4;
+    }
+    .main {
+        @apply px-2 bg-gray-100 h-screen flex flex-col gap-2 items-start justify-center;
+    }
+    form {
+        @apply h-full mt-5;
+    }
+    form h2 {
+        @apply font-semibold text-lg text-secondary;
+    }
+</style>
