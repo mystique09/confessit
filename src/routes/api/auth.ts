9ef1f43cbd68b42cff1eb15d8ce8de3daf87269a
mystique09 <knightkrypto7@gmail.com>
2022-04-15 21:30:08 +0800

    add: auth api

---
+++ src/routes/api/auth.ts
@@ -0,0 +1,19 @@
+
+import { serialize } from "cookie";
+
+export async function post() {
+  return {
+    status: 201,
+    body: {
+      message: "Login Success."
+    },
+    headers: {
+      "Set-Cookie": serialize('auth', 'testcookie', {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      })
+    }
+  }
+}
