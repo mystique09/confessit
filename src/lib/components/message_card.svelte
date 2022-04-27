980e959566db87dcd2a598dbeda651bac22064d4
mystique09 <knightkrypto7@gmail.com>
2022-04-27 16:29:34 +0800

    add: message card

--- src/lib/components/message_card.svelte
+++ src/lib/components/message_card.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
 	type Message = {
 		id: number;
-		content: string;
+		to: string;
+		message: string;
 		created_at: string;
+		deleted_at: string;
 	};
 
 	export let message: Message;
 </script>
 
 <div class="card">
-	<h3>Received on {message.created_at}</h3>
-	<p>{message.content}</p>
+	<h3>Received on {new Date(message.created_at).toUTCString()}</h3>
+	<p>{message.message}</p>
 </div>
 
 <style lang="postcss">
 	.card {
-		@apply md:w-1/4 rounded-md w-full h-full bg-gray-600 mb-2 px-4 py-6;
+		@apply bg-purple-600 md:w-1/4 rounded-md w-full h-full mb-2 px-4 py-6;
 	}
 	h3 {
 		@apply font-bold text-xs text-gray-400;
 	}
 	p {
-		@apply overflow-scroll mt-4 font-normal text-white text-sm;
+		@apply overflow-scroll mt-4 font-normal text-gray-300 text-sm;
 	}
 </style>
-
