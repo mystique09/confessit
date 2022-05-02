0f4baed4969e1125f4776a3f5ef558302a6a88da
Benjie Ben Garcia <50514928+mystique09@users.noreply.github.com>
2022-05-02 12:41:03 +0800

    Update navbar.svelte

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -23,26 +23,26 @@
 
 <nav>
 	<div class="wrap">
-		<div class="inner-wrap">
-		<div class="logo">
-			<a href="/"><img src="/logo.svg" alt="ConfessIt Logo" /></a>
-		</div>
-		{#if !$session}
-			<a class="login_btn" href="/login">Login</a>
-		{:else}
-			<div class="menu_btn" on:click={() => (isOpen = !isOpen)}>
-				<MenuIcon />
-				<h4>Menu</h4>
+		<div class="iwrap">
+			<div class="logo">
+				<a href="/"><img src="/logo.svg" alt="ConfessIt Logo" /></a>
 			</div>
+			{#if !$session}
+				<a class="login_btn" href="/login">Login</a>
+			{:else}
+				<div class="menu_btn" on:click={() => (isOpen = !isOpen)}>
+					<MenuIcon />
+					<h4>Menu</h4>
+				</div>
+			{/if}
 		</div>
-			<div class="menu" class:hide={!isOpen}>
-				<ul>
-					<li><a href="/dashboard">Dashboard</a></li>
-					<li><a href="/dashboard/profile">My Account</a></li>
-					<li><button on:click={signOut} type="button">Sign out</button></li>
-				</ul>
-			</div>
-		{/if}
+		<div class="menu" class:hide={!isOpen}>
+			<ul>
+				<li><a href="/dashboard">Dashboard</a></li>
+				<li><a href="/dashboard/profile">My Account</a></li>
+				<li><button on:click={signOut} type="button">Sign out</button></li>
+			</ul>
+		</div>
 	</div>
 </nav>
 
@@ -77,7 +77,7 @@
 	.menu li {
 		@apply text-accent font-normal text-lg;
 	}
-	.inner-wrap {
+	.iwrap {
 		@apply flex flex-row items-center justify-between w-screen;
 	}
 </style>
