297985f4e607a5831dee22f66f4fd09a781d2536
Benjie Ben Garcia <50514928+mystique09@users.noreply.github.com>
2022-05-02 13:05:28 +0800

    add: footer font size

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,9 +1,12 @@
 <footer>
-    <a href="https://github.com/mystique09/mystique09" rel="noopener" terget="_blank">Github</a>
+    <a href="https://github.com/mystique09/mystique09" rel="noopener" target="_blank">My Github</a>
 </footer>
-
+    
 <style lang="postcss">
     footer {
         @apply w-screen max-w-4xl m-auto flex items-center;
+    }
+    a {
+        @apply font-semibold;
     }
 </style>
