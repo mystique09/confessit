ec4ea685d79ecf0dca6c515cf92fad8ad71b2e74
mystique09 <knightkrypto7@gmail.com>
2022-05-02 13:51:46 +0800

    remove: footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,7 @@
 <script>
 	import Navbar from '$lib/components/navbar.svelte';
-	import Footer from '$lib/components/footer.svelte';
 	import '../app.css';
 </script>
 
 <Navbar />
 <slot />
-<Footer />
