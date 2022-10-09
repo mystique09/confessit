ed57bf85be550ce81a55ba0159569aab65834dc9
mystique09 <knightkrypto7@gmail.com>
2022-10-10 01:11:23 +0800

    fix: set cookie to null if user wants to logout

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,11 +1,8 @@
-import { json } from '@sveltejs/kit';
-
-export async function POST({setHeaders, request}) {
-  setHeaders({
-    cookie: {
-      name: null,
+export async function POST({cookies, request}) {
+  cookies.set("Set-Cookie", null, {
       maxAge: -1,
-    }
+      path: '/',
   });
+  console.log(request.headers)
   return new Response('Logged out!');
 }
