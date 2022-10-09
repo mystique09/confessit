efef6f130ba149cfbeb2fe90b19459c5eb6404a1
mystique09 <knightkrypto7@gmail.com>
2022-10-10 00:49:59 +0800

    add: use parent instead to get the session from root layout

---
+++ src/routes/login/+page.ts
@@ -0,0 +1,11 @@
+import {redirect} from '@sveltejs/kit';
+
+export async function load({parent}) {
+  const {user} = await parent();
+  
+  if(!!user) {
+    throw redirect(302, '/dashboard');
+  }
+  
+  return {};
+}
