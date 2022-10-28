a6f2c1a62376729eebf526783f495435a1a7b035
mystique09 <knightkrypto7@gmail.com>
2022-10-28 23:28:34 +0800

    chore: remove, use server load instead

--- src/routes/+page.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export async function load({parent}) {
-	const {user} = await parent();
-	if (!!user) {
-		throw redirect(302, '/dashboard');
-	}
-	return {};
-}
