d2b606e012d130c87ce5bcd04d2a09b0f6dec095
mystique09 <knightkrypto7@gmail.com>
2022-10-28 23:31:44 +0800

    add: server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,9 @@
-import type { MaybePromise, RequestEvent, RequestOptions } from "@sveltejs/kit/types/private";
-import { parse } from "cookie";
+import type { Handle } from "@sveltejs/kit";
 
-/**@type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }: { event: RequestEvent, resolve: (event: RequestEvent, opts?: RequestOptions) => MaybePromise<Response> }) {
-  const cookies = parse(event.request.headers.get('cookie') || "");
-  
-  event.locals.user = cookies.cookie ? JSON.parse(cookies.cookie) : null;
+export const handle: Handle = async ({ event, resolve }) => {
+    const cookies = event.cookies.get("session") || "{}";
+    event.locals.session = JSON.parse(cookies);
 
-  const response = await resolve(event);
-  return response;
+    const response = await resolve(event);
+    return response;
 }
