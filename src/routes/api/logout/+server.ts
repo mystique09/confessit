ff69e7200e6c96789c9cb907b2e267aef7a3b8e9
mystique09 <knightkrypto7@gmail.com>
2022-10-28 23:28:20 +0800

    chore: removed, unused

--- src/routes/api/logout/+server.ts
+++
@@ -1,8 +0,0 @@
-export async function POST({cookies, request}) {
-  cookies.set("cookie", null, {
-      maxAge: -1,
-      path: '/',
-  });
-  console.log(request.headers)
-  return new Response('Logged out!');
-}
