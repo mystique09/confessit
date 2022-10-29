f0b37f5488ae94ec5180796cd0c115d1a64cf375
mystique09 <knightkrypto7@gmail.com>
2022-10-29 23:40:58 +0800

    feat: navbar color and size

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -23,7 +23,7 @@
 				<a href="/"><img src="/logo.svg" alt="ConfessIt Logo" /></a>
 			</div>
 			{#if !$page.data?.authenticated}
-				<a class="login_btn" href="/login">Sign in</a>
+				<a class="login_btn" href="/login">Login</a>
 			{:else}
 				<div
 					class="menu_btn"
@@ -34,49 +34,58 @@
 				</div>
 			{/if}
 		</div>
-		<div class="menu" class:hide={!isOpen}>
-			<ul>
-				<li on:click={showNav} on:keydown={showNav}><a href="/dashboard">Dashboard</a></li>
-				<li on:click={showNav} on:keydown={showNav}><a href="/dashboard/profile">My Account</a></li>
-				<li on:click={showNav} on:keydown={showNav}>
-					<button on:click={signOut} type="button">Sign out</button>
-				</li>
-			</ul>
-		</div>
+	</div>
+	<div class="menu" class:hide={!isOpen}>
+		<ul>
+			<li on:click={showNav} on:keydown={showNav}><a href="/dashboard">Dashboard</a></li>
+			<li on:click={showNav} on:keydown={showNav}><a href="/dashboard/profile">My Account</a></li>
+			<li on:click={showNav} on:keydown={showNav}>
+				<button on:click={signOut} type="button">Sign out</button>
+			</li>
+		</ul>
 	</div>
 </nav>
 
 <style lang="postcss">
 	nav {
-		@apply max-w-4xl m-auto flex items-center;
+		@apply w-full flex flex-col items-center;
 	}
+
 	.wrap {
-		@apply px-4 py-2 w-screen flex flex-col items-start justify-between gap-1;
+		@apply max-w-4xl m-auto px-4 py-2 w-full flex flex-col items-start justify-between gap-1;
 	}
+
 	.logo {
 		@apply w-16 h-12;
 	}
+
 	.logo a img {
 		@apply w-full h-full;
 	}
+
 	.login_btn {
-		@apply bg-secondary/10;
+		@apply bg-secondary/20;
 		@apply ring ring-secondary rounded-sm border-none;
 		@apply font-normal text-xs text-secondary;
 		@apply px-8 py-2;
 	}
+
 	.menu_btn {
 		@apply h-full cursor-pointer flex flex-row items-center justify-between gap-1 text-secondary;
 	}
+
 	.hide {
 		@apply hidden;
 	}
+
 	.menu {
-		@apply px-4 py-2 bg-gray-100 w-full;
+		@apply px-4 py-2 w-full shadow-lg;
 	}
+
 	.menu li {
-		@apply text-accent font-normal text-lg;
+		@apply text-black/80 font-normal text-lg;
 	}
+
 	.iwrap {
 		@apply flex flex-row gap-4 items-center justify-between w-full;
 	}
