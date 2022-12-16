c7b02d050112dc5875c8ebb44cef4f604cb8bb1f
mystique09 <knightkrypto7@gmail.com>
2022-12-16 17:04:25 +0800

    add: showcase_card component

---
+++ src/lib/components/home/showcase_card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import MessageCard from "../message/message_card.svelte";
+
+	let messages: boolean[] = [false, false, false, true, true, true, true, true, true];
+
+</script>
+<div class="card max-w-sm w-full h-auto bg-white">
+	<div class="card-body">
+		<h1 class="card-title flex items-center w-full justify-center">
+			<img class="h-16" src="favicon.png" alt="CNFS Logo" />
+		</h1>
+		<div class="flex flex-wrap gap-2 items-center justify-start">
+			{#each messages as message}
+				<MessageCard isOpen={message} />
+			{/each}
+		</div>
+	</div>
+</div>
