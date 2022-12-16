9d2ae25f3a945fb19ef325751e0a47fd106b1457
mystique09 <knightkrypto7@gmail.com>
2022-12-16 17:04:58 +0800

    add: back_to_top component

---
+++ src/lib/components/back_to_top.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Reload from "./icons/reload.svelte";
+
+</script>
+<div class="fixed bottom-2 md:bottom-4 right-2 md:right-6">
+	<a href="/" class="btn btn-error-content btn-outline btn-circle btn-md">
+		<Reload className="w-12 h-1/2" />
+	</a>
+</div>
