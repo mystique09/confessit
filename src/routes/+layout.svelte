dcdcedda987d46b9827360bc8e257f5cae310dd3
mystique09 <knightkrypto7@gmail.com>
2022-12-16 17:02:40 +0800

    add: layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
-<script>
-	import Navbar from '$lib/components/navbar.svelte';
+<script lang="ts">
+	import Navbar from '$lib/components/heading/navbar.svelte';
 	import '../app.css';
+	import '@fontsource/poppins';
+	import BackToTop from '$lib/components/back_to_top.svelte';
+
+	let yPos: number = 0;
 </script>
 
 <Navbar />
 <slot />
+
+<svelte:window bind:scrollY={yPos}></svelte:window>
+
+{#if yPos > 1000}
+	<BackToTop />
+{/if}
