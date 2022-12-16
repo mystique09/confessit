8471898fd19ebe3b1c5bc16be400abaee34d8d64
mystique09 <knightkrypto7@gmail.com>
2022-12-17 03:22:38 +0800

    feat: Menu and animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,29 @@
 	import '../app.css';
 	import '@fontsource/poppins';
 	import BackToTop from '$lib/components/back_to_top.svelte';
+	import toggleMenu, { toggle } from '$lib/store/menu';
+	import Menu from '$lib/components/heading/menu.svelte';
+	import { fade } from 'svelte/transition';
 
 	let yPos: number = 0;
+	let width: number = 0;
+
+	$: if (width > 760) {
+		$toggleMenu = false;
+	}
 </script>
 
 <Navbar />
-<slot />
 
-<svelte:window bind:scrollY={yPos}></svelte:window>
+{#if !$toggleMenu}
+	<div in:fade={{ duration: 100, delay: 300 }}>
+		<slot />
+	</div>
+{:else}
+	<Menu />
+{/if}
+
+<svelte:window bind:scrollY={yPos} bind:innerWidth={width} />
 
 {#if yPos > 1000}
 	<BackToTop />
