cb53ceea6ec3342f7e8f7c865bb40229819d74e3
mystique09 <knightkrypto7@gmail.com>
2022-12-16 17:02:16 +0800

    feat: format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,23 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {
-			colors: {
-				primary: "#FFEBF4",
-				secondary: "#FF338F",
-				accent: "#F104B6"
-			}
-		}
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	daisyui: {
+		themes: [
+			{
+				cnfsTheme: {
+					primary: "#f41199",
+					secondary: "#95bfed",
+					accent: "#006ce8",
+					neutral: "#1A1A2D",
+					"base-100": "#222A4E",
+					info: "#9ABCEF",
+					success: "#1F8E85",
+					warning: "#B58712",
+					error: "#EC2258",
+				},
+			},
+		],
 	},
-
-	plugins: []
+	plugins: [require("daisyui")],
 };
 
 module.exports = config;
