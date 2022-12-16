03e07a309dd355688f65c536c22373cb7885c1ab
mystique09 <knightkrypto7@gmail.com>
2022-12-16 17:02:09 +0800

    chore: removed, need to organize the directory

--- src/lib/variables.ts
+++
@@ -1,3 +0,0 @@
-export const variables = {
-    BACKEND_URL: import.meta.env.VITE_BACKEND_URL
-}
--- src/routes/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-export async function load({ locals }) {
-    return {
-        authenticated: !!locals.session?.token,
-        name: locals.session.name
-    };
-}
