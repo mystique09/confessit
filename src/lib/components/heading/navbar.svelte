5de68485bd9282cee1ea4b226547e3b229697b7b
mystique09 <knightkrypto7@gmail.com>
2022-12-16 17:04:33 +0800

    add: navbar, new design

---
+++ src/lib/components/heading/navbar.svelte
@@ -0,0 +1,22 @@
+<div class="navbar px-4 max-w-6xl m-auto">
+	<div class="w-auto flex-0">
+		<div>
+			<a href="/">
+				<img width="120px" height="120px" src="/favicon.png" alt="CNFS logo" />
+			</a>
+		</div>
+	</div>
+	<div class="navbar-center flex-1 items-center justify-center">
+		<ul class="hidden md:flex md:flex-row justify-around w-1/2 font-bold text-white">
+			<li><a href="/#whatiscnfs">What's CNFS</a></li>
+			<li><a href="/#guidelines">Guidelines</a></li>
+			<li><a href="/#faq">FAQ</a></li>
+		</ul>
+	</div>
+	<div class="flex-0">
+		<a
+			class="btn btn-error px-10 h-14 text-sm normal-case rounded-full font-bold hover:bg-transparent hover:text-error"
+			href="/sign-up">JOIN CNFS</a
+		>
+	</div>
+</div>
