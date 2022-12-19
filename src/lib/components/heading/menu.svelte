5a5008d0d4be88bed541621befd66f20927e6c67
mystique09 <knightkrypto7@gmail.com>
2022-12-19 17:19:40 +0800

    add: logout button

--- src/lib/components/heading/menu.svelte
+++ src/lib/components/heading/menu.svelte
@@ -1,44 +1,78 @@
 <script lang="ts">
-	import { toggle } from '$lib/store/menu';
+	import { invalidateAll } from '$app/navigation';
+	import toggleMenu, { toggle } from '$lib/store/menu';
 	import { fly } from 'svelte/transition';
 	import Close from '../icons/close.svelte';
+
+	export let isAuthenticated: boolean = false;
 
 	type navLink = {
 		name: string;
 		href: string;
-	}
+	};
 
 	let navLinks: navLink[] = [
 		{
 			name: 'Wall',
-			href: '/wall',
+			href: '/wall'
 		},
 		{
 			name: 'What is CNFS?',
-			href: '/#whatiscnfs',
+			href: '/#whatiscnfs'
 		},
 		{
 			name: 'Guidelines',
-			href: '/#guidelines',
+			href: '/#guidelines'
 		},
 		{
 			name: 'FAQ',
-			href: '/#faq',
-		},
+			href: '/#faq'
+		}
 	];
+
+	const signoutHandler = async () => {
+		try {
+			const req = await fetch('/logout', {
+				method: 'POST'
+			});
+			const data = await req.json();
+			console.log(data);
+			await invalidateAll();
+		} catch (e) {
+			console.log(e);
+		}
+		$toggleMenu = false;
+	};
 </script>
 
-<div in:fly={{duration: 500, x: 500}} out:fly={{duration: 200, x: 500}} class="sidebar fixed top-0 left-0 bg-neutral w-screen h-screen z-10 md:hidden">
+<div
+	in:fly={{ duration: 500, x: 500 }}
+	out:fly={{ duration: 200, x: 500 }}
+	class="sidebar fixed top-0 left-0 bg-neutral w-screen h-screen z-10 md:hidden"
+>
 	<div class="wrap flex flex-col justify-evenly gap-1 h-full p-4">
 		<button class="btn btn-square btn-ghost flex-0 self-end" on:click={toggle}>
 			<Close className="stroke-white w-6 h-6" />
 		</button>
 		<ul class="w-full h-auto flex flex-col items-center justify-evenly flex-1">
 			{#each navLinks as navLink}
-				<li on:click={toggle} on:keydown={toggle} class="btn btn-wide btn-md rounded-full font-light normal-case">
+				<li
+					on:click={toggle}
+					on:keydown={toggle}
+					class="btn btn-wide btn-md rounded-full font-light normal-case"
+				>
 					<a class="w-full" href={navLink.href}>{navLink.name}</a>
 				</li>
 			{/each}
+			{#if isAuthenticated}
+				<li>
+					<button
+						on:click={signoutHandler}
+						type="button"
+						class="normal-case btn btn-wide btn-accent">Sign out</button
+					>
+				</li>
+			{/if}
 		</ul>
 	</div>
 </div>
--- src/lib/components/heading/navbar.svelte
+++ src/lib/components/heading/navbar.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-	import MenuIcon from "./menu_icon.svelte";
-	import {toggle} from "$lib/store/menu";
+	import MenuIcon from './menu_icon.svelte';
+	import { toggle } from '$lib/store/menu';
+	import { invalidateAll } from '$app/navigation';
+
+	export let isAuthenticated: boolean = false;
+
+	const signoutHandler = async () => {
+		try {
+			const req = await fetch('/logout', {
+				method: 'POST'
+			});
+			const data = await req.json();
+			console.log(data);
+			await invalidateAll();
+		} catch (e) {
+			console.log(e);
+		}
+	};
 </script>
 
 <div class="navbar px-4 max-w-6xl m-auto">
@@ -11,21 +27,29 @@
 			</a>
 		</div>
 	</div>
-	<div class="navbar-center flex-1 items-center justify-center">
-		<ul class="hidden md:flex md:flex-row justify-around w-1/2 font-bold text-white">
-			<li><a href="/wall">Wall</a></li>
-			<li><a href="/#whatiscnfs">What's CNFS</a></li>
-			<li><a href="/#guidelines">Guidelines</a></li>
-			<li><a href="/#faq">FAQ</a></li>
-		</ul>
-	</div>
-	<div class="hidden md:flex md:flex-0">
-		<a
-			class="btn btn-error px-10 h-14 text-sm normal-case rounded-full font-bold hover:bg-transparent hover:text-error"
-			href="/sign-up">JOIN CNFS</a
-		>
-	</div>
-	<div class="navbar-end md:hidden">
+	{#if !isAuthenticated}
+		<div class="navbar-center flex-1 items-center justify-center">
+			<ul class="hidden md:flex md:flex-row justify-evenly w-1/2 font-bold text-white">
+				<li><a href="/wall">Wall</a></li>
+				<li><a href="/#whatiscnfs">What's CNFS</a></li>
+				<li><a href="/#guidelines">Guidelines</a></li>
+				<li><a href="/#faq">FAQ</a></li>
+			</ul>
+		</div>
+		<div class="hidden md:flex md:flex-0">
+			<a
+				class="btn btn-error px-10 h-14 text-sm normal-case rounded-full font-bold hover:bg-transparent hover:text-error"
+				href="/sign-up">JOIN CNFS</a
+			>
+		</div>
+	{:else}
+		<div class="navbar-end hidden md:flex md:flex-1">
+			<button on:click={signoutHandler} type="button" class="btn btn-accent normal-case btn-wide"
+				>Sign out</button
+			>
+		</div>
+	{/if}
+	<div class="navbar-end md:hidden flex-1">
 		<button class="btn btn-square btn-ghost" on:click={toggle}>
 			<MenuIcon />
 		</button>
