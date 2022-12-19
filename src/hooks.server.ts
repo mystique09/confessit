3c255337f5856fa7439633658bdcadd3780bfc27
mystique09 <knightkrypto7@gmail.com>
2022-12-19 17:15:04 +0800

    add: server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,17 @@
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const cookies = event.cookies.get("session") || "{}";
-    event.locals.session = JSON.parse(cookies);
+    const sessionId = event.cookies.get("session_id") ?? "";
+	const accessToken = event.cookies.get("access_token") ?? "";
+	const refreshToken = event.cookies.get("refresh_token") ?? "";
+	const user = JSON.parse(event.cookies.get("user") ?? "{}");
+	const userIdentity = JSON.parse(event.cookies.get("user_identity") ?? "{}");
+
+	event.locals.session_id = sessionId;
+	event.locals.access_token = accessToken;
+	event.locals.refresh_token = refreshToken;
+	event.locals.user = user;
+	event.locals.user_identity = userIdentity;
 
     const response = await resolve(event);
     return response;
