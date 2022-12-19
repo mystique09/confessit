9e361f5909060ab74cb853bdf430f48945f977e5
mystique09 <knightkrypto7@gmail.com>
2022-12-19 17:18:52 +0800

    add: dashbaord page layout

--- src/routes/(auth)/dashboard/+page.svelte
+++ src/routes/(auth)/dashboard/+page.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+
 <svelte:head>
 	<title>CNFS - Dashboard</title>
 	<meta name="description" content="Welcome to your dashboard" />
 </svelte:head>
 
 <h1>dashboard</h1>
+<p>Welcome to dashboard, {data.user.username ?? ""}</p>
