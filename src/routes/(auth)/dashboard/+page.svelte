93ab86ac51d9fcd6b9ae35c29e4fc3c349f9e601
mystique09 <knightkrypto7@gmail.com>
2022-12-20 23:30:56 +0800

    feat: render data

--- src/routes/(auth)/dashboard/+page.svelte
+++ src/routes/(auth)/dashboard/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
+	import MessageCard from '$lib/components/dashboard/message_card.svelte';
+	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let messages = data.messages;
+	$: messages = messages;
 </script>
 
 <svelte:head>
@@ -9,5 +13,23 @@
 	<meta name="description" content="Welcome to your dashboard" />
 </svelte:head>
 
-<h1>dashboard</h1>
-<p>Welcome to dashboard, {data.user.username ?? ""}</p>
+<div class="dashboard w-full h-screen bg-white py-6 px-4">
+	<h1 class="text-neutral text-2xl md:text-4xl mb-8">Dashboard</h1>
+	<div class="flex items-center justify-start flex-wrap gap-4">
+		{#if messages.length == 0}
+			<div class="text-neutral text-xl md:text-2xl">
+				<h1 class="text-2xl">No messages</h1>
+				<p class="text-sm">You can start by sharing your link to your friends.</p>
+			</div>
+		{:else}
+			{#each messages as message (message.id + message.content)}
+				<MessageCard
+					content={message.content}
+					date={message.created_at}
+					messageId={message.id}
+					isOpen={!!message.seen}
+				/>
+			{/each}
+		{/if}
+	</div>
+</div>
