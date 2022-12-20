dff7360abae4a2ddf973b460cd21451c468c9945
mystique09 <knightkrypto7@gmail.com>
2022-12-20 23:30:19 +0800

    add: MessageModal component

---
+++ src/lib/components/dashboard/message_modal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Delete from '../icons/delete.svelte';
+	import Share from '../icons/share.svelte';
+
+	export let messageId: string;
+	export let content: string;
+	export let date: string;
+</script>
+
+<label for={messageId} class="modal cursor-pointer flex flex-col gap-1">
+	<label class="modal-box relative h-1/2 bg-neutral" for="">
+		<span class="text-xs">{new Date(date).toUTCString()}</span>
+		<p class="text-xl flex items-center justify-center h-3/4 fo	nt-bold">{content}</p>
+	</label>
+	<div class="modal-action flex flex-row items-center justify-evenly">
+		<button class="btn btn-error">
+			<Delete />
+		</button>
+		<button class="btn btn-success">
+			<Share />
+		</button>
+	</div>
+</label>
