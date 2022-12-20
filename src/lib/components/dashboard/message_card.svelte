ea8f1e37a5177fd98b7945d2356ed34379caada5
mystique09 <knightkrypto7@gmail.com>
2022-12-20 23:30:08 +0800

    add: MessageCard component

---
+++ src/lib/components/dashboard/message_card.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import EnvelopeClose from '../icons/envelope_close.svelte';
+	import EnvelopeOpen from '../icons/envelope_open.svelte';
+	import MessageModal from './message_modal.svelte';
+
+	export let isOpen: boolean = false;
+	export let messageId: string;
+	export let content: string;
+	export let date: string;
+</script>
+
+<label type="button" for={messageId} class="btn bg-base-100 w-20 h-20 md:w-24 md:h-24 shadow-lg">
+	<div class="relative h-auto w-full">
+		{#if !isOpen}
+			<div class="badge badge-error flex items-center badge-xs absolute -top-4 -right-4" />
+		{/if}
+		<div class="m-auto">
+			{#if isOpen}
+				<EnvelopeOpen className="w-full h-3/4" />
+			{:else}
+				<EnvelopeClose className="w-full h-3/4" />
+			{/if}
+		</div>
+	</div>
+</label>
+
+<input type="checkbox" id={messageId} class="modal-toggle" />
+<MessageModal {content} {date} {messageId} />
