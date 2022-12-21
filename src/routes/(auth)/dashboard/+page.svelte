b787d3762fdd10be04bac6917fd03421de5ff761
mystique09 <knightkrypto7@gmail.com>
2022-12-22 01:24:07 +0800

    add: delete and download function

--- src/routes/(auth)/dashboard/+page.svelte
+++ src/routes/(auth)/dashboard/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import MessageCard from '$lib/components/dashboard/message_card.svelte';
+	import messageStore from '$lib/store/messages';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-
-	let messages = data.messages;
-	$: messages = messages;
+	messageStore.initMessages(data.messages);
 </script>
 
 <svelte:head>
@@ -16,13 +15,13 @@
 <div class="dashboard w-full h-screen bg-base py-6 px-4">
 	<h1 class="text-white text-2xl md:text-4xl mb-8">Dashboard</h1>
 	<div class="flex items-center justify-start flex-wrap gap-4">
-		{#if messages.length == 0}
+		{#if $messageStore.length == 0}
 			<div class="text-white text-xl md:text-2xl">
 				<h1 class="text-2xl">No messages</h1>
 				<p class="text-sm">You can start by sharing your link to your friends.</p>
 			</div>
 		{:else}
-			{#each messages as message (message.id + message.content)}
+			{#each $messageStore as message (message.id + message.content)}
 				<MessageCard
 					content={message.content}
 					date={message.created_at}
