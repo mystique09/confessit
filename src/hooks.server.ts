8b393ca2033e0364b7fb994fe9e162bcd3c742b6
mystique09 <knightkrypto7@gmail.com>
2022-12-22 15:24:16 +0800

    feat: format

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,33 +1,34 @@
-import { redirect, type Handle } from "@sveltejs/kit";
+import { VITE_BACKEND_URL } from "$env/static/private";
+import { error, redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-	let sessionId = event.cookies.get("session_id") ?? "";
-	let accessToken = event.cookies.get("access_token") ?? "";
-	let refreshToken = event.cookies.get("refresh_token") ?? "";
-	let user = JSON.parse(event.cookies.get("user") ?? "{}");
-	let userIdentity = JSON.parse(event.cookies.get("user_identity") ?? "{}");
+    let sessionId = event.cookies.get("session_id") ?? "";
+    let accessToken = event.cookies.get("access_token") ?? "";
+    let refreshToken = event.cookies.get("refresh_token") ?? "";
+    let user = JSON.parse(event.cookies.get("user") ?? "{}");
+    let userIdentity = JSON.parse(event.cookies.get("user_identity") ?? "{}");
 
-	if (event.url.pathname.startsWith("/dashboard")) {
-		if (!(sessionId && accessToken && refreshToken && user && userIdentity)) {
-			throw redirect(301, "/sign-in");
-		}
-	}
+    if (event.url.pathname.startsWith("/dashboard")) {
+        if (!(sessionId && accessToken && refreshToken && user && userIdentity)) {
+            throw redirect(301, "/sign-in");
+        }
+    }
 
-	if (
-		event.url.pathname.startsWith("/sign") ||
-		event.url.pathname.startsWith("/forgot-password")
-	) {
-		if (sessionId && accessToken && refreshToken && user && userIdentity) {
-			throw redirect(301, "/dashboard");
-		}
-	}
+    if (
+        event.url.pathname.startsWith("/sign") ||
+        event.url.pathname.startsWith("/forgot-password")
+    ) {
+        if (sessionId && accessToken && refreshToken && user && userIdentity) {
+            throw redirect(301, "/dashboard");
+        }
+    }
 
-	event.locals.session_id = sessionId;
-	event.locals.access_token = accessToken;
-	event.locals.refresh_token = refreshToken;
-	event.locals.user = user;
-	event.locals.user_identity = userIdentity;
+    event.locals.session_id = sessionId;
+    event.locals.access_token = accessToken;
+    event.locals.refresh_token = refreshToken;
+    event.locals.user = user;
+    event.locals.user_identity = userIdentity;
 
-	const response = await resolve(event);
-	return response;
+    const response = await resolve(event);
+    return response;
 };
