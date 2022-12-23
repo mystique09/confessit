99e4c768b2fbc4ca0dacdd6d8e71732c28358e7f
mystique09 <knightkrypto7@gmail.com>
2022-12-24 02:04:06 +0800

    feat: check if the server returnred a non-json

--- src/routes/(auth)/sign-up/+page.server.ts
+++ src/routes/(auth)/sign-up/+page.server.ts
@@ -3,57 +3,62 @@ import { fail } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
-	signUp: async function ({ request, fetch }) {
-		const data = await request.formData();
-		const username = data.get("username");
-		const password = data.get("password");
-		const confirmPassword = data.get("confirm-password");
+    signUp: async function({ request, fetch }) {
+        const data = await request.formData();
+        const username = data.get("username");
+        const password = data.get("password");
+        const confirmPassword = data.get("confirm-password");
 
-		if (!(password && username && confirmPassword)) {
-			return fail(400, {
-				missingCredentials: true,
-				message: "Missing username, password, or confirmation password.",
-			});
-		}
+        if (!(password && username && confirmPassword)) {
+            return fail(400, {
+                missingCredentials: true,
+                message: "Missing username, password, or confirmation password.",
+            });
+        }
 
-		if (username.length < 8) {
-			return fail(400, {
-				invalidUsernameLength: true,
-				message: "Username must be at least 8 characters long.",
-			});
-		}
+        if (username.length < 8) {
+            return fail(400, {
+                invalidUsernameLength: true,
+                message: "Username must be at least 8 characters long.",
+            });
+        }
 
-		if (password.length < 8) {
-			return fail(400, {
-				invalidPasswordLength: true,
-				message: "Password must be at least 8 characters long.",
-			});
-		}
+        if (password.length < 8) {
+            return fail(400, {
+                invalidPasswordLength: true,
+                message: "Password must be at least 8 characters long.",
+            });
+        }
 
-		if (password !== confirmPassword) {
-			return fail(400, {
-				passwordsDoNotMatch: true,
-				message: "Passwords did not match.",
-			});
-		}
+        if (password !== confirmPassword) {
+            return fail(400, {
+                passwordsDoNotMatch: true,
+                message: "Passwords did not match.",
+            });
+        }
 
-		try {
-			const req = await fetch(`${VITE_BACKEND_URL}/api/v1/users`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ username, password }),
-			});
-			const res = await req.json();
-			if (req.status !== 200) {
-				return fail(req.status, { signUpFailed: true, message: res.message });
-			}
+        try {
+            const req = await fetch(`${VITE_BACKEND_URL}/api/v1/users`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
 
-			console.log(res);
-			return { success: true, message: "Successfully signed up." };
-		} catch (e) {
-			return fail(400, { signUpFailed: true, message: e.message });
-		}
-	},
+            if (req.status === 404) {
+                return fail(req.status, { loginFailed: true, message: "Server is offline." });
+            }
+
+            const res = await req.json();
+            if (req.status !== 200) {
+                return fail(req.status, { signUpFailed: true, message: res.message });
+            }
+
+            console.log(res);
+            return { success: true, message: "Successfully signed up." };
+        } catch (e) {
+            return fail(400, { signUpFailed: true, message: e.message });
+        }
+    },
 };
