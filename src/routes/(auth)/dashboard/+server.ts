9a6990d1eb0a5ab14295aa4adc108e42f601e5e6
mystique09 <knightkrypto7@gmail.com>
2022-12-24 00:57:00 +0800

    feat: implement reload button for fetching the latest data

--- src/routes/(auth)/dashboard/+server.ts
+++ src/routes/(auth)/dashboard/+server.ts
@@ -2,6 +2,38 @@ import { VITE_BACKEND_URL } from "$env/static/private";
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
+export const GET = (async({url, locals, fetch}) => {
+	const offset = url.searchParams.get("page");
+
+	try {
+		const req = await fetch(`${VITE_BACKEND_URL}/api/v1/users/${locals.user.id}/messages`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${locals.access_token}`
+			},
+		});
+
+		if (req.status !== 200) {
+			return json({error: "Unable fetch updated data."}, {
+				status: req.status
+			});
+		}
+
+		const res = await req.json();
+		const {data} = res;
+
+
+		return json(data, {
+			status: 200
+		});
+	} catch(e) {
+		return json({error: "Unable to fetch latest data."}, {
+			status: 500
+		});
+	}
+}) satisfies RequestHandler;
+
 export const PUT = (async ({request, locals, fetch}) => {
 	const body = await request.json();
 	const {access_token} = locals;
