ea94e8bbf9c1d9f4413b942cc8bba821e291347d
mystique09 <knightkrypto7@gmail.com>
2022-12-24 02:02:05 +0800

    add: serverStatus

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,33 +3,34 @@
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
 declare namespace App {
-	interface Locals {
-		session_id: string;
-		access_token: string;
-		refresh_token: string;
-		user: {
-			id: string;
-			username: string;
-			created_at: Date;
-			updated_at: Date;
-		};
-		user_identity: {
-			id: string;
-			user_id: string;
-			identity_hash: string;
-		};
-		authenticated: boolean;
-	}
-	// interface Platform {}
-	interface Session {}
-	// interface Stuff {}
+    interface Locals {
+        serverStatus: "online" | "offline";
+        session_id: string;
+        access_token: string;
+        refresh_token: string;
+        user: {
+            id: string;
+            username: string;
+            created_at: Date;
+            updated_at: Date;
+        };
+        user_identity: {
+            id: string;
+            user_id: string;
+            identity_hash: string;
+        };
+        authenticated: boolean;
+    }
+    // interface Platform {}
+    interface Session { }
+    // interface Stuff {}
 }
 
 type Message = {
-	id: string;
-	user_id: string;
-	content: string;
-	seen: boolean;
-	created_at: string;
-	updated_at: string;
+    id: string;
+    user_id: string;
+    content: string;
+    seen: boolean;
+    created_at: string;
+    updated_at: string;
 }
