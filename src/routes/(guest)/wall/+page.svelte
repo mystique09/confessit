c7cfe84e490ece9f56d46d149bae238a8e09de31
mystique09 <knightkrypto7@gmail.com>
2022-12-27 17:36:17 +0800

    add: layout for wall

--- src/routes/(guest)/wall/+page.svelte
+++ src/routes/(guest)/wall/+page.svelte
@@ -1,12 +1,52 @@
+<script lang="ts">
+	import CreatePost from '$lib/components/wall/create_post.svelte';
+	import Post from '$lib/components/wall/post.svelte';
+	import postStore from '$lib/store/posts';
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	onMount(() => {
+		postStore.initPosts(data.posts.data);
+	});
+</script>
+
 <svelte:head>
 	<title>CNFS - Public wall</title>
 	<meta name="description" content="Welcome to the CNFS public wall!" />
 </svelte:head>
 
-<div class="h-screen flex flex-col items-center justify-center px-4">
-	<h1 class="text-8xl m-7">🤖</h1>
-	<p class="font-bold text-4xl tracking-wide">Feature not available, under construction!</p>
-	<p class="text-sm md:text-center tracking-wide mt-2 max-w-2xl">
-		Thank you for your interest in CNFS. We are currently working on improving and updating this feature to provide a better experience for our users. We apologize for any inconvenience this may cause, and we appreciate your patience while we work to make CNFS even better. We will be sure to announce when the updated version of CNFS is ready for use. In the meantime, please feel free to explore the other features on our site or contact us with any questions or concerns. Thank you for your support!
-	</p>
+{#if data.authenticated}
+	<CreatePost />
+{/if}
+<div class="min-h-screen px-4 w-full">
+	<div class="wrap md:mt-12 w-full h-full lg:flex lg:flex-row lg:justify-evenly m-auto">
+		{#if $postStore.length > 0}
+			<div class="posts flex flex-col items-center h-full lg:w-1/2 gap-2">
+				{#each $postStore as post}
+					<Post item={post} />
+				{/each}
+			</div>
+		{:else}
+			<div class="flex flex-col items-center justify-center h-full">
+				<h1 class="text-xl lg:text-4xl">No post yet, be the first one!</h1>
+			</div>
+		{/if}
+		<div class="w-1/4">
+			<div class="guidelines_card h-56 w-full bg-neutral hidden lg:flex lg:flex-col p-4">
+				<ul class="text-xs flex flex-col gap-2">
+					<li><a class="hover:underline" href="/#privacy-policy">Privacy policy</a></li>
+					<li><a class="hover:underline" href="/#guidelines">Guidelines</a></li>
+					<li><a class="hover:underline" href="/#faq">FAQ</a></li>
+				</ul>
+				<div class="divider" />
+				<ul class="text-xs flex flex-col gap-2">
+					<a href="https://github.com/mystique09/confessit" class="link link-hover">Github</a>
+					<a href="https://twitter.com/voidthebug" class="link link-hover">Twitter</a>
+					<a href="https://github.com/mystique09" class="link link-hover">Linkedin</a>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
