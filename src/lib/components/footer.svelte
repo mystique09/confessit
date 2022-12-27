52049ddb8ba4cee95726d78bae35184529ba4555
mystique09 <knightkrypto7@gmail.com>
2022-12-27 17:37:28 +0800

    feat: twitter link

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -9,7 +9,7 @@
 	<div>
 		<span class="footer-title">SOCIALS</span> 
 		<a href="https://github.com/mystique09/confessit" class="link link-hover">Github</a> 
-		<a href="https://github.com/mystique09" class="link link-hover">Twitter</a> 
+		<a href="https://twitter.com/voidthebug" class="link link-hover">Twitter</a> 
 		<a href="https://github.com/mystique09" class="link link-hover">Linkedin</a> 
 	</div>
 
