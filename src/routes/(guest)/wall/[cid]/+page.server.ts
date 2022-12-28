3986567ca6378c2e969d50068bbcdf49849a4e50
mystique09 <knightkrypto7@gmail.com>
2022-12-28 12:12:45 +0800

    add: action for comment

--- src/routes/(guest)/wall/[cid]/+page.server.ts
+++ src/routes/(guest)/wall/[cid]/+page.server.ts
@@ -1,5 +1,6 @@
 import { VITE_BACKEND_URL } from "$env/static/private";
-import { error } from "@sveltejs/kit";
+import { error, fail } from "@sveltejs/kit";
+import type { Actions } from "./$types";
 import type { PageServerLoad } from "./$types";
 
 
@@ -25,3 +26,41 @@ export const load = (async ({params, fetch, locals}) => {
 		throw error(404, {message: "not found"})
 	}
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	newComment: async ({params, request, fetch, locals}) => {
+		const {cid} = params;
+		const data = await request.formData();
+		const comment = data.get("comment").toString();
+
+		if(!comment) {
+			return fail(400, {commentFailed: true, message: "comment is required"});
+		}
+
+		try {
+			const req = await fetch(`${VITE_BACKEND_URL}/api/v1/comments`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${locals.access_token}`,
+				},
+				body: JSON.stringify({
+					content: comment,
+					parent_id: "00000000-0000-0000-0000-000000000000",
+					post_id: cid,
+					user_identity_id: locals.user_identity.id,
+				})
+			});
+
+			if(req.status !== 200) {
+				return fail(req.status, {commentFailed: true, message: "comment failed"});
+			}
+
+			const res = await req.json();
+
+			return {commentSuccess: true, message: "comment success"};
+		} catch(err) {
+			return fail(400, {commentFailed: true, message: "comment failed"})
+		}
+	}
+};
