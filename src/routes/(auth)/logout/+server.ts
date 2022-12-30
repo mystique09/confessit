13fe3d9731b97533852de9ac757810a1452bbf72
mystique09 <knightkrypto7@gmail.com>
2022-12-30 12:30:52 +0800

    feat: session_id in locals

--- src/routes/(auth)/logout/+server.ts
+++ src/routes/(auth)/logout/+server.ts
@@ -2,9 +2,10 @@ import { VITE_BACKEND_URL } from "$env/static/private";
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
-export const POST = (async ({ cookies, fetch }) => {
+export const POST = (async ({ cookies, fetch, locals }) => {
+	const sessionId = locals.session_id;
+
     try {
-        const sessionId = cookies.get("session_id");
         const req = await fetch(`${VITE_BACKEND_URL}/api/v1/auth/clear`, {
             method: "DELETE",
             headers: {
@@ -13,31 +14,30 @@ export const POST = (async ({ cookies, fetch }) => {
             body: JSON.stringify({ session_id: sessionId })
         });
 
-        if (req.status !== 200) {
-            cookies.delete("session_id", {
-                path: '/',
-                maxAge: -1
-            });
-            cookies.delete("access_token", {
-                path: '/',
-                maxAge: -1
-            });
-            cookies.delete("refresh_token", {
-                path: '/',
-                maxAge: -1
-            });
-            cookies.delete("user", {
-                path: '/',
-                maxAge: -1
-            });
-            cookies.delete("user_identity", {
-                path: '/',
-                maxAge: -1
-            });
-            return json({ mesasge: "cookies deleted" });
-        }
-
         cookies.delete("session_id", {
+            path: '/',
+            maxAge: -1
+        });
+        cookies.delete("access_token", {
+            path: '/',
+            maxAge: -1,
+        });
+        cookies.delete("refresh_token", {
+            path: '/',
+            maxAge: -1
+        });
+        cookies.delete("user", {
+            path: '/',
+            maxAge: -1
+        });
+        cookies.delete("user_identity", {
+            path: '/',
+            maxAge: -1
+        });
+
+        return json({ mesasge: "cookies deleted" });
+    } catch (e) {
+		cookies.delete("session_id", {
             path: '/',
             maxAge: -1
         });
@@ -58,8 +58,6 @@ export const POST = (async ({ cookies, fetch }) => {
             maxAge: -1
         });
 
-        return json({ mesasge: "cookies deleted" });
-    } catch (e) {
         return json({ message: e.message }, {
             status: e.status
         });
