dbd56a77e86e81a8d42801662c2c98bf2ebb7932
mystique09 <knightkrypto7@gmail.com>
2023-01-01 16:04:45 +0800

    add: loading screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	import toggleMenu from '$lib/store/menu';
 	import Menu from '$lib/components/heading/menu.svelte';
 	import { fade } from 'svelte/transition';
-	import { page } from '$app/stores';
+	import { navigating, page } from '$app/stores';
 
 	let yPos: number = 0;
 	let width: number = 0;
@@ -14,20 +14,43 @@
 	$: if (width > 760) {
 		$toggleMenu = false;
 	}
+
+	let showLoadingScreen = false;
+
+	$: {
+		if ($navigating) {
+			showLoadingScreen = true;
+		}
+
+		if (!$navigating) {
+			showLoadingScreen = false;
+		}
+	}
 </script>
 
-{#if $page.data.serverStatus === "offline"}
-	<div class="fixed z-10 badge badge-xl badge-warning w-full text-lg">😦 Server is {$page.data.serverStatus}</div>
+{#if $page.data.serverStatus === 'offline'}
+	<div class="fixed z-10 badge badge-xl badge-warning w-full text-lg">
+		😦 Server is {$page.data.serverStatus}
+	</div>
 {/if}
 
 <Navbar isAuthenticated={$page.data.authenticated} />
 
-{#if !$toggleMenu}
-	<div in:fade={{ duration: 100, delay: 300 }}>
-		<slot />
+{#if showLoadingScreen}
+	<div class="fixed z-30 top-0 left-0 w-screen h-screen bg-neutral">
+		<div class="container container-md h-full m-auto flex flex-col items-center justify-center">
+			<span class="text-lg">Loading data</span>
+			<progress class="mt-2 progress progress-primary w-56" />
+		</div>
 	</div>
 {:else}
-	<Menu isAuthenticated={$page.data.authenticated} />
+	{#if !$toggleMenu}
+		<div in:fade={{ duration: 100, delay: 200 }}>
+			<slot />
+		</div>
+	{:else}
+		<Menu isAuthenticated={$page.data.authenticated} />
+	{/if}
 {/if}
 
 <svelte:window bind:scrollY={yPos} bind:innerWidth={width} />
