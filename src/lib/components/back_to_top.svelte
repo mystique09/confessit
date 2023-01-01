f877e357b0b491c811b1520ab74878f8d177f5af
mystique09 <knightkrypto7@gmail.com>
2023-01-01 16:04:41 +0800

    feat: back to top

--- src/lib/components/back_to_top.svelte
+++ src/lib/components/back_to_top.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a
-	href="/"
+	href="/#"
 	class="fixed bottom-6 md:bottom-12 right-6 md:right-12 btn btn-error-content btn-outline btn-circle btn-md z-20"
 >
 	<ArrowUp className="w-12 h-1/2" />
