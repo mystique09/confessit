0b3760a5e9a1260a1f97dc017a4d2224f33ce3f3
mystique09 <knightkrypto7@gmail.com>
2023-01-04 22:03:09 +0800

    add: recaptcha

--- src/routes/(auth)/sign-up/+page.server.ts
+++ src/routes/(auth)/sign-up/+page.server.ts
@@ -1,64 +1,91 @@
-import { VITE_BACKEND_URL } from "$env/static/private";
+import { RECAPTCHA_SECRET_KEY, VITE_BACKEND_URL } from "$env/static/private";
 import { fail } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
-    signUp: async function({ request, fetch, locals }) {
-        if (locals.serverStatus === "offline") {
-            return fail(404, { signUpFailed: true, message: "Server is offline." });
-        }
+	signUp: async function ({ request, fetch, locals }) {
+		if (locals.serverStatus === "offline") {
+			return fail(404, { signUpFailed: true, message: "Server is offline." });
+		}
 
-        const data = await request.formData();
-        const username = data.get("username");
-        const password = data.get("password");
-        const confirmPassword = data.get("confirm-password");
+		const data = await request.formData();
+		const username = data.get("username");
+		const password = data.get("password");
+		const confirmPassword = data.get("confirm-password");
+		const gRecaptchaToken = data.get("token");
 
-        if (!(password && username && confirmPassword)) {
-            return fail(400, {
-                missingCredentials: true,
-                message: "Missing username, password, or confirmation password.",
-            });
-        }
+		if (!(password && username && confirmPassword)) {
+			return fail(400, {
+				missingCredentials: true,
+				message: "Missing username, password, or confirmation password.",
+			});
+		}
 
-        if (username.length < 8) {
-            return fail(400, {
-                invalidUsernameLength: true,
-                message: "Username must be at least 8 characters long.",
-            });
-        }
+		if (username.length < 8) {
+			return fail(400, {
+				invalidUsernameLength: true,
+				message: "Username must be at least 8 characters long.",
+			});
+		}
 
-        if (password.length < 8) {
-            return fail(400, {
-                invalidPasswordLength: true,
-                message: "Password must be at least 8 characters long.",
-            });
-        }
+		if (password.length < 8) {
+			return fail(400, {
+				invalidPasswordLength: true,
+				message: "Password must be at least 8 characters long.",
+			});
+		}
 
-        if (password !== confirmPassword) {
-            return fail(400, {
-                passwordsDoNotMatch: true,
-                message: "Passwords did not match.",
-            });
-        }
+		if (password !== confirmPassword) {
+			return fail(400, {
+				passwordsDoNotMatch: true,
+				message: "Passwords did not match.",
+			});
+		}
 
-        try {
-            const req = await fetch(`${VITE_BACKEND_URL}/api/v1/users`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username, password }),
-            });
+		if (!gRecaptchaToken) {
+			return fail(400, {
+				missingRecaptchaToken: true,
+				message: "Missing reCAPTCHA token.",
+			});
+		}
 
-            const res = await req.json();
-            if (req.status !== 200) {
-                return fail(req.status, { signUpFailed: true, message: res.message });
-            }
+		try {
+			const verifyRequest = await fetch(
+				`https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${gRecaptchaToken}`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
+				},
+			);
 
-            console.log(res);
-            return { success: true, message: "Successfully signed up." };
-        } catch (e) {
-            return fail(400, { signUpFailed: true, message: e.message });
-        }
-    },
+			const verifyResponse = await verifyRequest.json();
+
+			if (!verifyResponse.success) {
+				return fail(400, {
+					invalidRecaptchaToken: true,
+					message: "Invalid reCAPTCHA token.",
+				});
+			}
+
+			const req = await fetch(`${VITE_BACKEND_URL}/api/v1/users`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ username, password }),
+			});
+
+			const res = await req.json();
+			if (req.status !== 200) {
+				return fail(req.status, { signUpFailed: true, message: res.message });
+			}
+
+			console.log(res);
+			return { success: true, message: "Successfully signed up." };
+		} catch (e) {
+			return fail(400, { signUpFailed: true, message: e.message });
+		}
+	},
 };
--- src/routes/(auth)/sign-up/+page.svelte
+++ src/routes/(auth)/sign-up/+page.svelte
@@ -7,7 +7,8 @@
 	import Key from '$lib/components/icons/key.svelte';
 	import UserAvatar from '$lib/components/icons/user_avatar.svelte';
 	import type { ActionData } from './$types';
-	import {slide} from "svelte/transition";
+	import { slide } from 'svelte/transition';
+	import Check from '$lib/components/icons/check.svelte';
 
 	let showPassword: boolean = false;
 	let submitForm: boolean = false;
@@ -22,23 +23,48 @@
 
 	export let form: ActionData;
 
-	$: if(form?.success) {
-		if(browser) {
+	$: if (form?.success) {
+		if (browser) {
 			goto('/sign-in').then(() => {
 				window.location.reload();
 			});
 		}
 	}
 
-	$: if(form?.signUpFailed || form?.invalidPasswordLength || form?.invalidUsernameLength || form?.passwordsDoNotMatch || form?.missingCredentials) {
+	$: if (
+		form?.signUpFailed ||
+		form?.invalidPasswordLength ||
+		form?.invalidUsernameLength ||
+		form?.passwordsDoNotMatch ||
+		form?.missingCredentials ||
+		form?.missingRecaptchaToken ||
+		form?.invalidRecaptchaToken
+	) {
 		submitForm = false;
 	}
+
+	$: token = '';
+
+	const onSubmitCaptcha = () => {
+		grecaptcha.ready(function () {
+			grecaptcha
+				.execute('6LdBec4jAAAAAFDaErCHEhir_-q3bK5ytmRxb5FZ', { action: 'button' })
+				.then(function (t: string) {
+					// Add your logic to submit to your backend server here.
+					console.log(t);
+					token = t;
+				});
+		});
+	};
 </script>
 
 <svelte:head>
 	<title>CNFS - Sign up</title>
 	<meta name="description" content="Join CNFS now!" />
-</svelte:head>
+	<script
+		src="https://www.google.com/recaptcha/api.js?render=6LdBec4jAAAAAFDaErCHEhir_-q3bK5ytmRxb5FZ"
+	></script></svelte:head
+>
 
 <div class="min-h-screen px-4">
 	<div class="h-full container mt-12 max-w-2xl m-auto bg-[#EEEEEE] px-4 py-6 rounded-md">
@@ -50,11 +76,17 @@
 			>
 				Google
 			</div>
-			<div class="btn btn-disabled w-full md:w-3/4 rounded btn-md text-white normal-case">Github</div>
+			<div class="btn btn-disabled w-full md:w-3/4 rounded btn-md text-white normal-case">
+				Github
+			</div>
 		</div>
 		<div class="divider w-3/4 m-auto my-4 text-neutral">or</div>
-		{#if form?.signUpFailed || form?.invalidPasswordLength || form?.invalidUsernameLength || form?.passwordsDoNotMatch || form?.missingCredentials}
-			<div in:slide={{delay: 300}} tabindex="-1" class="collapse w-full md:w-3/4 m-auto collapse-open">
+		{#if form?.signUpFailed || form?.invalidPasswordLength || form?.invalidUsernameLength || form?.passwordsDoNotMatch || form?.missingCredentials || form?.invalidRecaptchaToken || form?.missingRecaptchaToken}
+			<div
+				in:slide={{ delay: 300 }}
+				tabindex="-1"
+				class="collapse w-full md:w-3/4 m-auto collapse-open"
+			>
 				<div class="collapse-title text-error text-sm">Signup Error</div>
 				<div class="collapse-content text-xs">
 					<p class="text-error">{form?.message}</p>
@@ -104,15 +136,43 @@
 					id="confirm-password"
 				/>
 			</div>
-			<div class="flex flex-col md:flex-row-reverse items-center gap-2 m-auto w-full mt-4">
-				<button
-					on:click={submitFormHandler}
-					class:loading={submitForm}
-					class:btn-disabled={submitForm}
-					class:text-neutral={submitForm}
-					class="btn btn-error rounded-md w-full md:w-1/2 normal-case">Create your account</button
-				>
-				<a class="btn btn-ghost w-full text-neutral md:w-1/2 normal-case" href="/sign-in">Log in</a>
+			<input type="hidden" id="token" name="token" bind:value={token} />
+			<div class="flex-col items-start w-full gap-4 md:flex-row mt-2">
+				{#if form?.missingRecaptchaToken}
+					<button
+						type="button"
+						id="g-recaptcha"
+						name="g-recaptcha"
+						aria-required="true"
+						on:click={onSubmitCaptcha}
+						class="g-recaptcha btn btn-ghost text-error text-xs font-light normal-case"
+					>
+						<Check className="w-8 h-8 stroke-error" /> human verification
+					</button>
+				{:else}
+					<button
+						type="button"
+						id="g-recaptcha"
+						name="g-recaptcha"
+						aria-required="true"
+						on:click={onSubmitCaptcha}
+						class="g-recaptcha btn btn-ghost text-neutral text-xs font-light normal-case"
+					>
+						<Check className="w-8 h-8 stroke-success" /> human verification
+					</button>
+				{/if}
+				<div class="flex flex-col md:flex-row-reverse items-center gap-2 m-auto w-full mt-4">
+					<button
+						on:click={submitFormHandler}
+						class:loading={submitForm}
+						class:btn-disabled={submitForm}
+						class:text-neutral={submitForm}
+						class="btn btn-error rounded-md w-full md:w-1/2 normal-case">Create your account</button
+					>
+					<a class="btn btn-ghost w-full text-neutral md:w-1/2 normal-case" href="/sign-in"
+						>Log in</a
+					>
+				</div>
 			</div>
 		</form>
 	</div>
