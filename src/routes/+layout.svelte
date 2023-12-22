d0d512297fa4d0e6fde7039b670212b330e1db72
mystique09 <knightkrypto7@gmail.com>
2023-12-23 03:14:47 +0800

    chore: declare fallback directly in children

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,14 @@
 	import BackToTop from '$lib/components/layout/back_to_top.svelte';
 	import Menu from '$lib/components/layout/menu.svelte';
 	import Navbar from '$lib/components/layout/navbar.svelte';
+	import Show from '$lib/components/shared/show.svelte';
 	import toggleMenu from '$lib/store/menu';
 	import '@fontsource/poppins';
-	import { setContext } from 'svelte';
+	import { setContext, type Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import '../app.css';
+
+	let { children } = $props<{children: Snippet}>();
 
 	let yPos: number = $state(0);
 	let width: number = $state(0);
@@ -33,39 +36,42 @@
 	setContext('userAuth', { isAuthenticated: $page.data.isAuthenticated ?? false });
 </script>
 
-{#if $page.data.serverStatus === 'offline'}
-	<div class="fixed z-10 badge badge-xl badge-warning w-full text-lg">
-		😦 Server is {$page.data.serverStatus}
-	</div>
-{/if}
-
-<Navbar />
-
-{#if showLoadingScreen}
-	<div class="fixed z-30 top-0 left-0 w-screen h-screen bg-neutral">
-		<div class="container container-md h-full m-auto flex flex-col items-center justify-center">
-			<span class="text-lg">Loading data</span>
-			<progress class="mt-2 progress progress-primary w-56" />
-		</div>
-	</div>
-{:else if !$toggleMenu}
-	<div in:fade|global={{ duration: 100, delay: 200 }}>
-		<slot />
-	</div>
-{:else}
-	<Menu />
-{/if}
-
 <svelte:window bind:scrollY={yPos} bind:innerWidth={width} />
-
 <svelte:head>
 	<script
 		src={`https://www.google.com/recaptcha/api.js?render=${PUBLIC_RECAPTCHA_KEY}`}
 	></script></svelte:head
 >
 
-{#if !$page.url.pathname.startsWith('/wall')}
-	{#if yPos > 1000}
-		<BackToTop />
-	{/if}
-{/if}
+<Show when={$page.data.serverStatus === 'offline'}>
+	<div class="fixed z-10 badge badge-xl badge-warning w-full text-lg">
+		😦 Server is {$page.data.serverStatus}
+	</div>
+</Show>
+
+<Navbar />
+
+<Show when={showLoadingScreen}>
+	<div class="fixed z-30 top-0 left-0 w-screen h-screen bg-neutral">
+		<div class="container container-md h-full m-auto flex flex-col items-center justify-center">
+			<span class="text-lg">Loading data</span>
+			<progress class="mt-2 progress progress-primary w-56" />
+		</div>
+	</div>
+
+	{#snippet fallback()}
+		<Show when={$toggleMenu}>
+			<Menu />
+
+			{#snippet fallback()}
+				<div in:fade|global={{ duration: 100, delay: 200 }}>
+					{@render children()}
+				</div>
+			{/snippet}
+		</Show>
+	{/snippet}
+</Show>
+
+<Show when={!$page.url.pathname.startsWith('/wall') && yPos > 1000}>
+	<BackToTop />
+</Show>
